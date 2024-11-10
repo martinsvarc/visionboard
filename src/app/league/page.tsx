67725fb572ffa84775232cf6ea7564d511e97fcc
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-10 06:29:48 +0100

    Update page.tsx

--- src/app/league/page.tsx
+++ src/app/league/page.tsx
@@ -21,6 +21,7 @@ type LeagueData = {
   last_call_date: string;
   score_history: number[];
   rank: number;
+ profile_picture_url: string;
 }
 
 type CategoryNames = {
@@ -218,17 +219,18 @@ export default function Component() {
                         )}
                       </div>
                       <div className={`
-                        relative w-8 h-8 rounded-full overflow-hidden
-                        ${rankColor}
-                      `}>
-                        <Image
-                          src="/placeholder.svg?height=32&width=32"
-                          alt={user.user_name}
-                          width={32}
-                          height={32}
-                          className="object-cover"
-                        />
-                      </div>
+                      <div className={`
+  relative w-8 h-8 rounded-full overflow-hidden
+  ${rankColor}
+`}>
+  <Image
+    src={user.profile_picture_url} // Update this line
+    alt={user.user_name}
+    width={32}
+    height={32}
+    className="object-cover"
+  />
+</div>
                       <div className={`
                         flex-1 text-sm font-medium flex items-center gap-2
                         ${rank <= 3
