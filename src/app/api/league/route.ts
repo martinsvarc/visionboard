9e6730a4ac3904ddd57edf0462332078fd6acce2
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-11 05:51:33 +0100

    Update route.ts

--- src/app/api/league/route.ts
+++ src/app/api/league/route.ts
@@ -1,6 +1,9 @@
 import { createPool } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export const GET = async (request: Request) => {
   try {
     const { searchParams } = new URL(request.url);
