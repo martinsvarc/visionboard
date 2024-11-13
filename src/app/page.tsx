45228855f87bd1f30fad27e1a0811da6e455078a
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-13 22:08:07 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="text-2xl">
-        <a href="/vision-board" className="text-blue-500 hover:text-blue-600">
-          Go to Vision Board
-        </a>
+      <div className="space-y-4 text-2xl">
+        <div>
+          <a href="/vision-board" className="text-blue-500 hover:text-blue-600">
+            Go to Vision Board
+          </a>
+        </div>
+        <div>
+          <a href="/analytics" className="text-blue-500 hover:text-blue-600">
+            Go to Analytics Dashboard
+          </a>
+        </div>
       </div>
     </main>
-  )
+  );
 }
