7ad3a188d17f0f9d6411c1b1dbb7a66868fa97e8
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-26 08:34:08 +0100

    Update vision-board-dashboard.tsx

--- src/app/mrdky/vision-board-dashboard.tsx
+++ src/app/mrdky/vision-board-dashboard.tsx
@@ -10,7 +10,6 @@ import { cn } from "@/lib/utils"
 import { CustomCalendar } from "@/components/custom-calendar"
 import { LeagueChart } from '@/components/LeagueChart'
 import { AchievementContent } from './achievement-showcase';
-import type { Badge } from '@/lib/achievement-data';
 
 interface LeaguePlayer {
   rank: number
@@ -45,6 +44,17 @@ interface VisionItem {
   height: number
   zIndex: number
   aspectRatio: number
+}
+
+interface Badge {
+  id: string;
+  image: string;
+  description?: string;
+  tooltipTitle: string;
+  tooltipSubtitle: string;
+  unlocked?: boolean;
+  current?: number;
+  target?: number;
 }
 
 const getMemberId = async () => {
