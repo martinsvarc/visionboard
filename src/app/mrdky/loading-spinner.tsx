43ba76b2a46f5253b289979954ae79c5bc642e9d
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-15 13:28:23 +0100

    Update loading-spinner.tsx

--- src/app/mrdky/loading-spinner.tsx
+++ src/app/mrdky/loading-spinner.tsx
@@ -1,10 +1,9 @@
 'use client'
-
 import Image from 'next/image'
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center w-full h-screen bg-transparent">
+    <div className="fixed inset-0 w-full h-full min-h-[100vh] flex items-center justify-center bg-[#f0f1f7] z-[9999]">
       <div className="relative w-24 h-24" role="status" aria-label="Loading">
         {/* Spinning loader */}
         <svg
