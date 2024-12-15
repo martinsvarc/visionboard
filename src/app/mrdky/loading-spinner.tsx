c0de5c987e8deb785038fb471d844c4e3b8bf079
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-15 12:53:31 +0100

    Update loading-spinner.tsx

--- src/app/mrdky/loading-spinner.tsx
+++ src/app/mrdky/loading-spinner.tsx
@@ -9,7 +9,7 @@ const LoadingSpinner = () => {
         {/* Spinning loader */}
         <svg
           className="w-full h-full animate-spin"
-          viewBox="0 0 100 100"
+          viewBox="0 0 400 400"
           xmlns="http://www.w3.org/2000/svg"
         >
           {/* Background circle */}
